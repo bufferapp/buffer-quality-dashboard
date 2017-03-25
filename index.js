ea3567981a40bbcbb1a2d1d26c09f814ddbccd2c
KatieLo <katielouw@gmail.com>
2017-03-25 12:16:04 -0700

    make responsive layout

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var path = __dirname + '/views/';
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('assets'))
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.sendFile(path + "index.html");
