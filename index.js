84f27803659fb79588087ed90da6185994f3dba4
KatieLo <katielouw@gmail.com>
2017-03-25 13:05:31 -0700

    console log number of open issues

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
 var express = require('express')
+var _ = require('underscore');
 var app = express()
 var path = __dirname + '/views/';
-const token = process.env.GITHUB_TOKEN;
+var GitHubApi = require('github');
+var githubToken;
 
-if (!token) {
+const PORT = process.env.PORT || 3000;
+
+if (process.env.ENV === 'production') {
+    githubToken = process.env.GITHUB_TOKEN;
+} else {
     try {
-        const token = require('./githubToken.json');
+        githubToken = require('./githubToken.json');
     }
     catch(error) {
-        console.log('No Github token');
+        console.error('No GitHub token found');
         return;
     }
 }
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static('assets'))
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.sendFile(path + "index.html");
+
+    var args = {
+        owner: 'bufferapp',
+        repo: 'buffer-web',
+        filter: 'open',
+        state: 'open',
+        since: (new Date(0)).toISOString(),
+        per_page: 100
+    };
+
+    github.issues.getForRepo(args, function(err, issues) {
+
+        issues = _.filter(issues.data, function(issue) {
+          return (!issue.pull_request);
+        });
+
+        console.log(issues.length);
+        res.sendFile(path + "index.html");
+    });
+
 })
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT + '!')
+    console.log('Example app listening on port ' + PORT + '!')
 })
+
+const github = new GitHubApi({
+    // optional
+    debug: false,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    pathPrefix: "", // for some GHEs; none for GitHub
+    headers: {
+        "user-agent": "Buffer-GitHub-Client" // GitHub is happy with a unique user agent
+    },
+    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+});
+github.authenticate({
+  type: 'oauth',
+  token: githubToken
+});
